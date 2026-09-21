70b3e0575aaa9a93cdc6d99a21345ac28afd8ed0
Bence Mihucza <mihben@gmail.com>
2026-09-21 16:36:16 +0200

    chore: Integrate Ironyx Kernel

--- src/Ironyx.ServiceIndex.BFF/Program.cs
+++ src/Ironyx.ServiceIndex.BFF/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSerilog((_, configuration) => configuration.ReadFrom.Configuration(builder.Configuration));
 
+builder.UseKernel()
+    .AddGrpc(5000)
+    .AddCommandSender(new Uri("http://localhost:5100/"));
+
 var app = builder.Build();
 
 app.Run();
